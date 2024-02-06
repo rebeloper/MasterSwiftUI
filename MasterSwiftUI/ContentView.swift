7911609de33412e70a34d111300bc21a1c7bb04e
Alex Nagy <rebeloperalpar@gmail.com>
2024-02-06 15:36:22 +0200

    session 1

--- MasterSwiftUI/ContentView.swift
+++ MasterSwiftUI/ContentView.swift
@@ -10,15 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+            Color.red
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+//        .preferredColorScheme(.dark)
 }
