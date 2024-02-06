5623ea275503e4a32c36f9dfa02b7e35a0e7c1dd
Alex Nagy <rebeloperalpar@gmail.com>
2024-02-06 15:09:30 +0200

    Initial Commit

---
+++ MasterSwiftUI/MasterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterSwiftUIApp.swift
+//  MasterSwiftUI
+//
+//  Created by Alex Nagy on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MasterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
