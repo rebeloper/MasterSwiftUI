4ee84f45c54b80ef080dd3b9770d57ef20223bd0
Alex Nagy <rebeloperalpar@gmail.com>
2024-04-09 15:59:39 +0300

    session 10

--- MasterSwiftUI/ContentView.swift
+++ MasterSwiftUI/ContentView.swift
@@ -13,55 +13,34 @@
 // Text
 // List
 // ScrollView, Menu, PhotosPicker, ProgressView, RenameButton
+// Color Picker, Date Picker, Disclosure Group, Form
 
 import SwiftUI
-import PhotosUI
 
 struct ContentView: View {
     
-    @State private var persons = [
-        Person(name: "Ada"),
-        Person(name: "Bob"),
-        Person(name: "Claire")
-    ]
-    
-    struct Person: Identifiable, Hashable {
-        var id = UUID().uuidString
-        
-        var name: String
-    }
-    
-    @State private var selection: [PhotosPickerItem] = []
-    @State private var selectedImages: [UIImage] = []
-    
-    @State private var progress = 0.30
-    @State private var total = 0.70
-    
-    @State private var editText = false
-    @State private var text = "Bob"
+    @State private var expanded = false
     
     var body: some View {
-        VStack {
-            if editText {
-                HStack {
-                    TextField("Name", text: $text)
-                        .textFieldStyle(.roundedBorder)
-                    Button("Done") {
-                        editText.toggle()
-                    }
-                }
-            } else {
-                HStack {
-                    Text(text)
-                    Spacer()
-                    RenameButton()
-                        .buttonStyle(.borderedProminent)
-                }
+        Form {
+            DisclosureGroup("Disclosure") {
+                Text("hey")
             }
-        }
-        .padding()
-        .renameAction {
-            editText.toggle()
+            Text("Hello")
+                .listRowBackground(Color.red)
+                .listRowInsets(.init(top: 30, leading: 30, bottom: 30, trailing: 30))
+            Color.orange
+            
+            Section {
+                Text("Sections")
+                Button("Button") {
+                    
+                }
+                Toggle("Toggle", isOn: .constant(true))
+            } header: {
+                Text("Header")
+            }
+            .headerProminence(.increased)
         }
     }
 
@@ -70,23 +49,5 @@ struct ContentView: View {
 #Preview {
     NavigationStack {
         ContentView()
-    }
-}
-
-extension PhotosPickerItem {
-    
-    @MainActor
-    func convert() async -> UIImage? {
-        do {
-            if let data = try await self.loadTransferable(type: Data.self) {
-                if let uiImage = UIImage(data: data) {
-                    return uiImage
-                }
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
-        
-        return nil
     }
 }
