5767868bd845803710bd8e4baff783c7517cc298
Alex Nagy <rebeloperalpar@gmail.com>
2024-07-16 15:57:36 +0300

    session 19

---
+++ MasterSwiftUI/CircularShapesView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularShapesView.swift
+//  MasterSwiftUI
+//
+//  Created by Alex Nagy on 15.07.2024.
+//
+
+import SwiftUI
+
+struct CircularShapesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularShapesView()
+}
