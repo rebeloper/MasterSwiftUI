284888fda03d07370615e83fac912c519e818201
Alex Nagy <rebeloperalpar@gmail.com>
2024-09-09 15:39:50 +0300

    session 27

---
+++ MasterSwiftUI/AsyncAwaitView.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncAwaitView.swift
+//  MasterSwiftUI
+//
+//  Created by Alex Nagy on 09.09.2024.
+//
+
+import SwiftUI
+
+struct AsyncAwaitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncAwaitView()
+}
